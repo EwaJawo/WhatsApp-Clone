da6bc8fb4b896aa420d4f7e747f671d69da1374f
Ewa Jaworska <ewa.grzelarczyk90@gmail.com>
2021-06-02 09:50:55 +0200

    profile information

--- app/src/main/java/com/example/whatsappclone/activities/ProfileActivity.java
+++ app/src/main/java/com/example/whatsappclone/activities/ProfileActivity.java
@@ -2,31 +2,72 @@ package com.example.whatsappclone.activities;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.whatsappclone.R;
 import com.example.whatsappclone.fragments.BottomSheetSelectImage;
+import com.example.whatsappclone.models.User;
+import com.example.whatsappclone.providers.AuthProvider;
+import com.example.whatsappclone.providers.UsersProvider;
 import com.example.whatsappclone.utils.MyToolbar;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileActivity extends AppCompatActivity {
 
     FloatingActionButton mFabSelectImage;
     BottomSheetSelectImage mBottomSheetSelectImage;
+    UsersProvider mUsersProvider;
+    AuthProvider mAuthProvider;
+
+    TextView mTextViewUsername;
+    TextView mTextViewPhone;
+    CircleImageView mCircleImageProfile;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         MyToolbar.show(this,"Profil",true);
+        mUsersProvider = new UsersProvider();
+        mAuthProvider = new AuthProvider();
+
+        mTextViewUsername = findViewById(R.id.textViewUsername);
+        mTextViewPhone = findViewById(R.id.textViewPhone);
+        mCircleImageProfile = findViewById(R.id.circleImageProfile);
 
         mFabSelectImage = findViewById(R.id.fabSelectImage);
         mFabSelectImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openBottomSheetSelectImage();
+            }
+        });
 
+        getUserInfo();
+    }
+
+    private void getUserInfo() {
+        mUsersProvider.getUserInfo(mAuthProvider.getId()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if (documentSnapshot.exists()) {
+                    User user = documentSnapshot.toObject(User.class);
+                    mTextViewUsername.setText(user.getUsername());
+                    mTextViewPhone.setText(user.getPhone());
+                    if(user.getImage() !=null) {
+                        if (!user.getImage().equals("")) {
+                            Picasso.with(ProfileActivity.this).load(user.getImage()).into(mCircleImageProfile);
+                        }
+                    }
+                }
             }
         });
     }
