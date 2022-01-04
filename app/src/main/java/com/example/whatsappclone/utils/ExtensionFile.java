25de370cbf19daebd5beea5c1f51b6feb9a1698a
EwaJawo <ewa.grzelarczyk90@gmail.com>
2022-01-04 12:13:54 +0100

    final version 04.01.22

---
+++ app/src/main/java/com/example/whatsappclone/utils/ExtensionFile.java
@@ -0,0 +1,41 @@
+package com.example.whatsappclone.utils;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.net.Uri;
+import android.webkit.MimeTypeMap;
+
+import java.io.File;
+import java.net.URLConnection;
+
+public class ExtensionFile {
+
+    public static boolean isImageFile(String path) {
+        String mimeType = URLConnection.guessContentTypeFromName(path);
+        return mimeType != null && mimeType.startsWith("image");
+    }
+
+    public static boolean isVideoFile(String path) {
+        String mimeType = URLConnection.guessContentTypeFromName(path);
+        return mimeType != null && mimeType.startsWith("video");
+    }
+
+    public static String getExtension(Context context, Uri uri) {
+        String extension;
+
+
+        if (uri.getScheme().equals(ContentResolver.SCHEME_CONTENT)) {
+            //If scheme is a content
+            final MimeTypeMap mime = MimeTypeMap.getSingleton();
+            extension = mime.getExtensionFromMimeType(context.getContentResolver().getType(uri));
+        } else {
+
+            extension = MimeTypeMap.getFileExtensionFromUrl(Uri.fromFile(new File(uri.getPath())).toString());
+
+        }
+
+        return extension;
+    }
+
+}
+
